c3a82fc4018ac005947cf30ca0e613dc84be0928
Julian Linne <julian.frederik.linne@student.uni-augsburg.de>
2023-04-09 19:11:08 +0200

    setup .gitignore

---
+++ src/main.cpp
@@ -0,0 +1,13 @@
+//
+// Created by Jul on 09.04.2023.
+//
+#include <iostream>
+
+int main()
+{
+    std::cout << "Hello World!";
+    for(int i = 0; i < 100; i++) {
+        printf("%i Quadrat ist %i\n", i, i * i);
+    }
+    return 0;
+}
